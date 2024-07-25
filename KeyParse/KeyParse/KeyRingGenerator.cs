90a98f67b6d2f816ea3132812ff6d5b8e4b068f6
lampsonnguyen <lampsonnguyen@yahoo.com>
2024-07-24 22:09:40 -0700

    add robust

--- KeyParse/KeyParse/KeyRingGenerator.cs
+++ KeyParse/KeyParse/KeyRingGenerator.cs
@@ -1,68 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+
 
 namespace KeyParse
 {
-
     public class KeyRingGenerator
     {
-        public static void GenerateTestFile(string filePath)
+        public static void GenerateTestFile(string filePath, uint keyValid, uint keyType, uint keyFormat, string keyName, uint keyECCurve, uint keyAESCipherType, uint keyAESCipherMode, uint keyIntegrityHashAlgorithm, uint keyLength)
         {
             using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
             {
-                // Key Valid (4 bytes)
-                writer.Write((uint)1); // VALID
+                // Prepare a MemoryStream to hold the binary data
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (BinaryWriter memoryWriter = new BinaryWriter(memoryStream))
+                    {
+                        // Key Valid (4 bytes)
+                        memoryWriter.Write(keyValid);
 
-                // Key Type (4 bytes)
-                writer.Write((uint)3); // AES
+                        // Key Type (4 bytes)
+                        memoryWriter.Write(keyType);
 
-                // Key Format (4 bytes)
-                writer.Write((uint)1); // RAW
+                        // Key Format (4 bytes)
+                        memoryWriter.Write(keyFormat);
 
-                // Key Name (32 bytes)
-                string keyName = "TestKey";
-                byte[] keyNameBytes = new byte[32];
-                Encoding.UTF8.GetBytes(keyName, 0, keyName.Length, keyNameBytes, 0);
-                writer.Write(keyNameBytes);
+                        // Key Name (32 bytes)
+                        byte[] keyNameBytes = new byte[32];
+                        Encoding.UTF8.GetBytes(keyName, 0, keyName.Length, keyNameBytes, 0);
+                        memoryWriter.Write(keyNameBytes);
 
-                // Key EC Curve (4 bytes)
-                writer.Write((uint)1); // P-384
+                        // Key EC Curve (4 bytes)
+                        memoryWriter.Write(keyECCurve);
 
-                // Key AES Cipher Type (4 bytes)
-                writer.Write((uint)3); // AES256
+                        // Key AES Cipher Type (4 bytes)
+                        memoryWriter.Write(keyAESCipherType);
 
-                // Key AES Cipher Mode (4 bytes)
-                writer.Write((uint)5); // CTR
+                        // Key AES Cipher Mode (4 bytes)
+                        memoryWriter.Write(keyAESCipherMode);
 
-                // Key Integrity Hash Algorithm (4 bytes)
-                writer.Write((uint)3); // SHA256
+                        // Key Integrity Hash Algorithm (4 bytes)
+                        memoryWriter.Write(keyIntegrityHashAlgorithm);
 
-                // Key Integrity Hash (64 bytes)
-                byte[] keyIntegrityHash = new byte[64];
-                new Random().NextBytes(keyIntegrityHash);
-                writer.Write(keyIntegrityHash);
+                        // Key Integrity Hash (64 bytes)
+                        byte[] keyIntegrityHash = new byte[64];
+                        new Random().NextBytes(keyIntegrityHash);
+                        memoryWriter.Write(keyIntegrityHash);
 
-                // Key Length (4 bytes)
-                writer.Write((uint)32); // 32 bytes
+                        // Key Length (4 bytes)
+                        memoryWriter.Write(keyLength);
 
-                // Key (144 bytes)
-                byte[] key = new byte[144];
-                new Random().NextBytes(key);
-                writer.Write(key);
+                        // Key (144 bytes)
+                        byte[] key = new byte[144];
+                        new Random().NextBytes(key);
+                        memoryWriter.Write(key);
 
-                // Key Reserved (240 bytes)
-                byte[] keyReserved = new byte[240];
-                new Random().NextBytes(keyReserved);
-                writer.Write(keyReserved);
+                        // Key Reserved (240 bytes)
+                        byte[] keyReserved = new byte[240];
+                        new Random().NextBytes(keyReserved);
+                        memoryWriter.Write(keyReserved);
 
-                // End (remaining bytes to make total 512 bytes)
-                long currentPosition = writer.BaseStream.Position;
-                long remainingBytes = 512 - currentPosition;
-                writer.Write(new byte[remainingBytes]);
+                        // End (remaining bytes to make total 512 bytes)
+                        long currentPosition = memoryStream.Position;
+                        long remainingBytes = 512 - currentPosition;
+                        memoryWriter.Write(new byte[remainingBytes]);
+
+                        // Print out the binary data
+                        byte[] binaryData = memoryStream.ToArray();
+                        Console.WriteLine("Binary Data for " + keyName + ":");
+                        Console.WriteLine(BitConverter.ToString(binaryData));
+
+                        // Write the binary data to the file
+                        writer.Write(binaryData);
+                    }
+                }
             }
         }
     }
+
 }
--- KeyParse/KeyParse/Program.cs
+++ KeyParse/KeyParse/Program.cs
@@ -4,24 +4,54 @@ class Program
 {
     static void Main()
     {
-        string filePath = @"C:\Users\lamps\OneDrive\Documents\GitHub\WixInstallerDemo\KeyParse\KeyParse\key_ring_test.bin";
-        KeyRingGenerator.GenerateTestFile(filePath);
-        Console.WriteLine("Test binary file created at: " + filePath);
+        string path = @"C:\Users\lamps\OneDrive\Documents\GitHub\WixInstallerDemo\KeyParse\KeyParse\";
+        // Example for AES key with CTR mode
+        GenerateAndPrint(path + "key_ring_aes_ctr.bin", 1, 3, 1, "AES_CTR_Key", 0, 3, 5, 3, 32);
 
-        byte[] binaryData = File.ReadAllBytes(filePath);
-        KeyData parsedData = BinaryParser.Parse(binaryData);
+        // Example for EC P-384 key
+        GenerateAndPrint(path + "key_ring_ec_p384.bin", 1, 1, 1, "EC_P384_Key", 1, 0, 0, 3, 96);
 
-        Console.WriteLine("Key Valid: " + parsedData.DecodeKeyValid());
-        Console.WriteLine("Key Type: " + parsedData.DecodeKeyType());
-        Console.WriteLine("Key Format: " + parsedData.DecodeKeyFormat());
-        Console.WriteLine("Key Name: " + parsedData.KeyName);
-        Console.WriteLine("Key EC Curve: " + parsedData.DecodeKeyECCurve());
-        Console.WriteLine("Key AES Cipher Type: " + parsedData.DecodeKeyAESCipherType());
-        Console.WriteLine("Key AES Cipher Mode: " + parsedData.DecodeKeyAESCipherMode());
-        Console.WriteLine("Key Integrity Hash Algorithm: " + parsedData.DecodeKeyIntegrityHashAlgorithm());
-        Console.WriteLine("Key Integrity Hash: " + BitConverter.ToString(parsedData.KeyIntegrityHash));
-        Console.WriteLine("Key Length: " + parsedData.KeyLength);
-        Console.WriteLine("Key: " + BitConverter.ToString(parsedData.Key));
-        Console.WriteLine("Key Reserved: " + BitConverter.ToString(parsedData.KeyReserved));
+        // Example for EC P-521 key
+        GenerateAndPrint(path + "key_ring_ec_p521.bin", 1, 1, 1, "EC_P521_Key", 2, 0, 0, 4, 132);
+
+        // Example for EC P-256 key
+        GenerateAndPrint(path + "key_ring_ec_p256.bin", 1, 1, 1, "EC_P256_Key", 3, 0, 0, 5, 64);
+
+        // Read and parse the files to demonstrate decoding
+        string[] filePaths = { "key_ring_aes_ctr.bin", "key_ring_ec_p384.bin", "key_ring_ec_p521.bin", "key_ring_ec_p256.bin" };
+
+        DisplayResult(path, filePaths);
+    }
+
+    private static void DisplayResult(string path, string[] filePaths)
+    {
+        foreach (var filePath in filePaths)
+        {
+            byte[] binaryData = File.ReadAllBytes(path + filePath);
+            KeyData parsedData = BinaryParser.Parse(binaryData);
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine($"Parsed Data from {filePath}:");
+            Console.WriteLine("Key Valid: " + parsedData.DecodeKeyValid());
+            Console.WriteLine("Key Type: " + parsedData.DecodeKeyType());
+            Console.WriteLine("Key Format: " + parsedData.DecodeKeyFormat());
+            Console.WriteLine("Key Name: " + parsedData.KeyName);
+            Console.WriteLine("Key EC Curve: " + parsedData.DecodeKeyECCurve());
+            Console.WriteLine("Key AES Cipher Type: " + parsedData.DecodeKeyAESCipherType());
+            Console.WriteLine("Key AES Cipher Mode: " + parsedData.DecodeKeyAESCipherMode());
+            Console.WriteLine("Key Integrity Hash Algorithm: " + parsedData.DecodeKeyIntegrityHashAlgorithm());
+            Console.WriteLine("Key Integrity Hash: " + BitConverter.ToString(parsedData.KeyIntegrityHash));
+            Console.WriteLine("Key Length: " + parsedData.KeyLength);
+            Console.WriteLine("Key: " + BitConverter.ToString(parsedData.Key));
+            Console.WriteLine("Key Reserved: " + BitConverter.ToString(parsedData.KeyReserved));
+            Console.WriteLine("------------------------------");
+        }
+    }
+
+    static void GenerateAndPrint(string filePath, uint keyValid, uint keyType, uint keyFormat, string keyName, uint keyECCurve, uint keyAESCipherType, uint keyAESCipherMode, uint keyIntegrityHashAlgorithm, uint keyLength)
+    {
+        Console.WriteLine("------------------------------");
+        KeyRingGenerator.GenerateTestFile(filePath, keyValid, keyType, keyFormat, keyName, keyECCurve, keyAESCipherType, keyAESCipherMode, keyIntegrityHashAlgorithm, keyLength);
+        Console.WriteLine($"Test binary file created at: {filePath}");
     }
 }
